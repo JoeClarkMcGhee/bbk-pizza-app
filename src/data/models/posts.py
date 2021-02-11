0376a50ff16c3efbfa0086596f3223c10a4930f0
JoeClarkMcGhee <joeclarkmcghee@yahoo.co.uk>
2021-02-11 12:18:55 +0000

    Add custom save to Post model
    We check that each post has at least one topic associated with it but not more than the maximum possible number of topics which is four.

--- src/data/models/posts.py
+++ src/data/models/posts.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import models as user_models
-from django.db import models
+from django.db import models, transaction
+
+from . import topics
 
 
 class Posts(models.Model):
@@ -13,3 +15,39 @@ class Posts(models.Model):
     author = models.ForeignKey(user_models.User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     body = models.TextField()
+
+    @transaction.atomic
+    def save(self, *args, **kwargs):
+        """
+        Custom save method that ensures we save at least one topic to a post.
+        """
+        topics_to_save = kwargs.get("topics")
+
+        # Check that topics have been supplied.
+        if not topics:
+            raise ValueError(
+                "You must provide a list of topics that associated to the post."
+            )
+
+        # Check they are of the correct type for us to operate on.
+        if type(topics) is not list:
+            raise ValueError("You must provide topics as a list")
+
+        #  Check for an empty or over sized list.
+        if len(topics_to_save) < 1 or len(topics_to_save) > 4:
+            raise ValueError("You must provide at least 1 topic but no more than 4")
+
+        # Validate that the topic type is legal.
+        try:
+            [topics.TopicType(topic) for topic in topics_to_save]
+        except ValueError as e:
+            raise Exception("Invalid topic types supplied") from e
+
+        # At this stage we are happy that the supplied topics are valid so we can save the post
+        post = super().save(*args, **kwargs)
+
+        # And then save the topics. We save the topics after the post as the topics have a
+        # foreign key to the post.
+        for topic in set(topics_to_save):
+            t = topics.Topics(topic=topic, post=post)
+            t.save()
