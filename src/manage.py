4ecc2a6a92b1c91845271a881ad0750bc08b34de
JoeClarkMcGhee <joeclarkmcghee@yahoo.co.uk>
2021-02-14 12:04:03 +0000

    Package the app (including the tests) using a setup file
    We then install the app (via pip) which has the affect of making all the modules in the various packages discoverable across the whole app.

--- src/manage.py
+++ src/manage.py
@@ -5,8 +5,5 @@ import sys
 from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
-    sys.path.append(os.path.dirname(sys.path[0]))
-    __package__ = "app"
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.pizza.settings")
     execute_from_command_line(sys.argv)
